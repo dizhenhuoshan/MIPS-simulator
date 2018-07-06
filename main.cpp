f8921ba3a6500dde33a67399b14f31eb944986b4
杨孟天 <lepetitprince517@sjtu.edu.cn>
2018-07-06 22:59:32 +0800

    finished

--- main.cpp
+++ main.cpp
@@ -6,7 +6,6 @@
 using namespace mips;
 int main(int argc, char **argv)
 {
-    freopen("18.in", "r", stdin);
     std::vector<mips::command> text_memory;
     std::map<std::string, unsigned int> data_label;
     std::map<std::string, label_info> text_label;
@@ -14,7 +13,7 @@ int main(int argc, char **argv)
     mips_register fake_register;
     unsigned int heap_pos = 0;
     unsigned int stack_pos = 6 * 1024 * 1024;
-    scanner mips_scanner("18.s", memory.get_bottom());
+    scanner mips_scanner(argv[1], memory.get_bottom());
     mips_scanner.scan_code(text_memory, memory.get_bottom(), heap_pos, data_label, text_label);
     fake_register.set_stack(stack_pos);
     decoder mips_decoder(text_label["main"].start_line, memory.get_bottom());
